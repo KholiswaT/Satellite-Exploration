d98bf986f2557fc216b782bef3633fae351cbb99
Asrit Caceres <asritcaceres@gmail.com>
2020-11-24 21:30:55 -0500

    adding python

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,20 +1,30 @@
 // Store endpoint inside queryUrl
-var queryUrl = "https://api.n2yo.com/rest/v1/satellite/above/41.702/-76.014/0/90/6/&apiKey=87BK3U-D8QTDU-BVBZ4W-4LL1";
 
-// Perform a GET request to the query URL
-d3.json(queryUrl, function(data) {
+queryUrl = "https://api.n2yo.com/rest/v1/satellite/positions/43719/40.0583/74.4057/0/1/&apiKey=87BK3U-D8QTDU-BVBZ4W-4LL1";
+console.log(queryUrl)
+// fetch(queryUrl)
+//   .then(data => console.log(info));
+// Fetch the JSON data and console log it
+// d3.json(url).then(function(data) {
+//   console.log(data);
+// });
+// // // Promise Pending
+// const dataPromise = d3.json(url);
+// console.log("Data Promise: ", dataPromise);
+
+
+// // Perform a GET request to the query URL
+d3.json(queryUrl,{mode:'cors', credentials:'include'}).then(function(data) {
   // send the data.features object to the createFeatures function
-  createFeatures(data.features);
-  console.log(data.features)
+  createFeatures(data.positions);
+  console.log(data.positions)
 });
-
-// function createFeatures(earthquakeData) {
-
-//   // Define function to run once for each feature in the features array
-//   // Give each feature a popup describing the place, time and magnitude of the earthquake
-//   function onEachFeature(feature, layer) {
-//     layer.bindPopup("<h3>" + feature.properties.place +
-//     "</h3><hr><p>" + new Date(feature.properties.time) + "</p>" +
-//     "</h3><hr><p>Magnitude:" +(feature.properties.mag) +"</p>" );
-//   }
+// d3.json(queryUrl, function(error, json) {
+//   if (error) return console.warn(error);
+//   data = json;
+//   visualizeit();
+// });
 //}
+
+// https://api.n2yo.com/rest/v1/satellite/positions/25544/41.702/-76.014/0/100/&apiKey=87BK3U-D8QTDU-BVBZ4W-4LL1
+
