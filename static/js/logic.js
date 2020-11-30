cce3b47952076f4a1d3029abfdfb42c9deb9f02f
Asrit Caceres <asritcaceres@gmail.com>
2020-11-30 09:55:28 -0500

    updating the syntax

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,7 +1,7 @@
 // Store endpoint inside queryUrl
 
-queryUrl = "https://api.n2yo.com/rest/v1/satellite/positions/43719/40.0583/74.4057/0/1/&apiKey=87BK3U-D8QTDU-BVBZ4W-4LL1";
-console.log(queryUrl)
+// queryUrl = "https://api.n2yo.com/rest/v1/satellite/positions/43719/40.0583/74.4057/0/1/&apiKey=87BK3U-D8QTDU-BVBZ4W-4LL1";
+//console.log(queryUrl)
 // fetch(queryUrl)
 //   .then(data => console.log(info));
 // Fetch the JSON data and console log it
@@ -14,11 +14,11 @@ console.log(queryUrl)
 
 
 // // Perform a GET request to the query URL
-d3.json(queryUrl,{mode:'cors', credentials:'include'}).then(function(data) {
-  // send the data.features object to the createFeatures function
-  createFeatures(data.positions);
-  console.log(data.positions)
-});
+// d3.json(queryUrl,{mode:'cors', credentials:'include'}).then(function(data) {
+//   // send the data.features object to the createFeatures function
+//   createFeatures(data.positions);
+//   console.log(data.positions)
+// });
 // d3.json(queryUrl, function(error, json) {
 //   if (error) return console.warn(error);
 //   data = json;
