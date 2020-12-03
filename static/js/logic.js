ec53758a2b3a6f414f827b403aee50d944912942
Asrit Caceres <asritcaceres@gmail.com>
2020-12-02 20:01:23 -0500

    maps

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,30 +1,38 @@
-// Store endpoint inside queryUrl
+var data
 
-// queryUrl = "https://api.n2yo.com/rest/v1/satellite/positions/43719/40.0583/74.4057/0/1/&apiKey=87BK3U-D8QTDU-BVBZ4W-4LL1";
-//console.log(queryUrl)
-// fetch(queryUrl)
-//   .then(data => console.log(info));
-// Fetch the JSON data and console log it
-// d3.json(url).then(function(data) {
-//   console.log(data);
-// });
-// // // Promise Pending
-// const dataPromise = d3.json(url);
-// console.log("Data Promise: ", dataPromise);
+function addMarkers() {
+  data.forEach(function(d) {
+    var marker = L.circleMarker([+d.lat, +d.long]);
+    marker.addTo(myMap);
+  })
+}
+// Creating map object
+var myMap = L.map("map", {
+  center: [40.54055668158878, -104.35709913347607],
+  zoom: 2
+});
+
+// Adding tile layer to the map
+L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+  tileSize: 512,
+  maxZoom: 18,
+  zoomOffset: -1,
+  id: "mapbox/streets-v11",
+  accessToken: API_KEY
+}).addTo(myMap);
 
 
-// // Perform a GET request to the query URL
-// d3.json(queryUrl,{mode:'cors', credentials:'include'}).then(function(data) {
-//   // send the data.features object to the createFeatures function
-//   createFeatures(data.positions);
-//   console.log(data.positions)
-// });
-// d3.json(queryUrl, function(error, json) {
-//   if (error) return console.warn(error);
-//   data = json;
-//   visualizeit();
-// });
-//}
+//Read data from CSV
+d3.csv("../../data/Satellite-Exploration-CSV.csv", function(csv) {
+  console.log(csv);
+  data = csv;
+  addMarkers();
+})
 
-// https://api.n2yo.com/rest/v1/satellite/positions/25544/41.702/-76.014/0/100/&apiKey=87BK3U-D8QTDU-BVBZ4W-4LL1
 
+//Store endpoint inside queryUrl
+//Fetch the JSON data and console log it
+//d3.json("../../app.json", function(data) {
+  //console.log(data);
+//})
