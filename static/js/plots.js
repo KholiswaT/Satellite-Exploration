600c9eafe6485ea2ce829768bd8b78d1bb3d8b75
Asrit Caceres <asritcaceres@gmail.com>
2020-12-07 21:34:43 -0500

    plot data count

--- static/js/plots.js
+++ static/js/plots.js
@@ -4,46 +4,66 @@
 d3.json("/satlaunch90.json").then((data) => {
   //console.log(data)
   
-  var yearsCount = {
-    Before_1970: 0,
-    From_1970to1980: 0,
-    From_1980to1990: 0,
-    From_1990to2000: 0,
-    From_2000to2010: 0,
-    From_2010toNow: 0,
-  };
-  var years = []
-  var year_list = []
-  for (var i = 0; i < data[0].above.length; i++){
-      //console.log(data[0].above[i]);
+  let Before_1970 = 0;
+  let From_1970to1980 =0;
+  let From_1980to1990=0;
+  let From_1990to2000 =0;
+  let From_2000to2010 =0;
+  let From_2010toNow=0;
+  // var Before_1970 = []
+  // var From_1970to1980=[]
+  // var From_1980to1990= []
+  // var From_1990to2000= []
+  // var From_2000to2010= []
+  // var From_2010toNow= []
+  for (let i = 0; i < data[0].above.length; i++) {
+   
+  // for (var i = 0; i < data[0].above.length; i++){
+  //     console.log(data[0].above[i]);
     years = [data[0].above[i].launchDate]
-      console.log(years)
+      //console.log(years)
       
   var years2 = years[0].slice(0,4)
-    console.log(years2)
-   
- 
-//    if (years2 <= "1970"){
-//           yearsCount.append.Before_1970;
-//       }
-//     else if(years <= "1980"){
-//           yearsCount= "From_1970to1980";
-//     }
-//     else if(years <= "1990"){
-//           yearsCount = "From_1980to1990";
-//     }
-//     else if(years < "2000"){
-//           yearsCount = "From_1990to2000";
-//     }
-//     else if(years < "2010"){
-//         yearsCount = "From_2000to2010";
-//     }
-//     else {
-//         yearsCount = "From_2010toNow"
-//     }
- 
-// console.log(yearsCount)
-// }})
+    //console.log(years2)
+    if (years2 <= "1970"){
+      //Before_1970.push(years2)
+      Before_1970++;
+      }
+    else if(years2 <= "1980"){
+      //From_1970to1980.push(years2)
+      From_1970to1980++;
+    }
+    else if(years2 <= "1990"){
+      //From_1980to1990.push(years2)
+      From_1980to1990++;
+      
+    }
+    else if(years2 <= "2000"){
+      //From_1990to2000.push(years2)
+      From_1990to2000++;
+    }
+    else if(years2 <= "2010"){
+     // From_2000to2010.push(years2)
+      From_2000to2010++;
+    }
+    else {
+      //From_2010toNow.push(years2)
+      From_2010toNow++;
+    }
+  }
+  console.log('Satellites before 1970:' + Before_1970)
+  console.log('Satellites between 1970 and 1980:' + From_1970to1980)
+  console.log('Satellites between 1980 and 1990:' + From_1980to1990)
+  console.log('Satellites between 1990 and 2000:' + From_1990to2000)
+  console.log('Satellites between 2000 and 2010:' + From_2000to2010)
+  console.log('Satellites after 2010:' + From_2010toNow)
+
+// var count = 0;
+// for(var i = 0; i < Before_1970.length; ++i){
+//     if(Before_1970 <= 1970)
+//         count++;
+//     console.log(count)
+  })
 
 //     //   var trace1 = {
     //     type: "scatter",
@@ -72,4 +92,4 @@ d3.json("/satlaunch90.json").then((data) => {
   
     //   Plotly.newPlot("plot", data, layout);
     // }}
-  }});
+  
